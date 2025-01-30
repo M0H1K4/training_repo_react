b520298bd2b07f15952473d8b1c626df93008d8a
M0H1K4N <nejer911@gmail.com>
2025-01-30 20:39:40 +0400

    adding

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const handleChange = () => {
+    setShowMenu(!showMenu);
+  };
+
+  return (
+    <>
+      <div onClick={handleChange}>{showMenu ?  'Hide menu' : 'show Menu'}</div>
+      {showMenu && (
+        <ul className="class">
+          <li>menu</li>
+          <li>123123</li>
+          <li>12312</li>
+          <li>31231</li>
+          <li>2312</li>
+        </ul>
+      )}
+    </>
+  );
+}
+
+export default App;
