a2728689730348b62e5dcf797b4c5d24a2237dee
M0H1K4N <nejer911@gmail.com>
2025-02-02 15:42:37 +0400

    Add

--- src/App.jsx
+++ src/App.jsx
@@ -2,28 +2,20 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+// import axios from 'axios'
+
 
 function App() {
-  const [showMenu, setShowMenu] = useState(false);
 
-  const handleChange = () => {
-    setShowMenu(!showMenu);
-  };
 
-  
+  const [counter, setCounter] = useState(0);
+  const increaseFunction = () => setCounter(counter + 1);
 
   return (
     <>
-      <div onClick={handleChange}>{showMenu ?  'Hide menu' : 'show Menu'}</div>
-      {showMenu && (
-        <ul className="class">
-          <li>menu</li>
-          <li>123123</li>
-          <li>12312</li>
-          <li>31231</li>
-          <li>2312</li>
-        </ul>
-      )}
+    hello
+      <p>{counter}</p>
+      <button onClick={increaseFunction}>increace button</button>
     </>
   );
 }
