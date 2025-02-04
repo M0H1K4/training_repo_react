87a0303a310b8a0498804c89a36995b406a2c70d
M0H1K4N <nejer911@gmail.com>
2025-02-04 21:13:49 +0400

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,58 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 // import axios from 'axios'
 
-
 function App() {
 
+  const [time, setTime] = useState(0);
 
-  const [counter, setCounter] = useState(0);
-  const increaseFunction = () => setCounter(counter + 1);
+    useEffect(() => {
+      const interval = setInterval(() => {
+        setTime((prevTime) => prevTime + 1)
+      }, 1000)
+
+      return () => clearInterval(interval)
+
+    })
+
+
 
   return (
     <>
-    hello
-      <p>{counter}</p>
-      <button onClick={increaseFunction}>increace button</button>
+
+      {/* <div className="board-row">
+        <Square value/>
+        <Square value/>
+        <Square value/>
+      </div>
+      <div className="board-row">
+        <Square value='4'/>
+        <Square value='5'/>
+        <Square value='6'/>
+      </div>
+      <div className="board-row">
+        <Square value='7'/>
+        <Square value='8'/>
+        <Square value='9'/>
+      </div> */}
+    <h1>time: {time}</h1>
+    
+    
     </>
   );
 }
 
 export default App;
+
+
+// function Square({value}){
+
+//   const [value, setValue] = useState(null)
+
+//   function handleClick(){
+//     console.log('clicked!');
+//   }
+//   return  <button onClick={handleClick} className="square">{value}</button>
+// }
